26bedfa0e73a4a1125b119ac052b2e099b290ce4
Christel <72789056+Christel-Mirassou@users.noreply.github.com>
2022-09-14 17:24:12 +0200

    Création de la classe Livre et création de 4 livres

--- livres.php
+++ livres.php
@@ -1,5 +1,14 @@
 <!-- Démarrage d'un buffer -->
-<?php ob_start() ?>
+<?php 
+require_once "Livre.class.php";
+ob_start(); 
+$l1 = new Livre(1, "Algorithmique selon H2PROG", 300, "algo.png");
+$l2 = new Livre(2, "Le virus Asiatique", 200, "virus.png");
+$l3 = new Livre(3, "La France du 19ème siècle", 100, "france.png");
+$l4 = new Livre(4, "Le JavaScript Client", 500, "JS.png");
+
+$livres = [$l1, $l2, $l3, $l4]
+?>
 
 <table class="table text-center">
   <thead>
@@ -11,20 +20,16 @@
     </tr>
   </thead>
   <tbody>
+    <?php for ($i=0; $i < count($livres); $i++) : ?>
     <tr>
-      <td class="align-middle"><img src="public/images/algo.png" alt="algo" width="60px;"></td>
-      <td class="align-middle">Algorithmique selon H2PROG</td>
-      <td class="align-middle">300</td>
+      <td class="align-middle"><img src="public/images/<?= $livres[$i]->getImage(); ?>" alt="algo" width="60px;"></td>
+      <td class="align-middle"><?= $livres[$i]->getTitre(); ?></td>
+      <td class="align-middle"><?= $livres[$i]->getNbPages(); ?></td>
       <td class="align-middle"><a href=""class="btn btn-warning">Modifier</a></td>
       <td class="align-middle"><a href=""class="btn btn-danger">Supprimer</a></td>
     </tr>
-    <tr>
-      <td class="align-middle"><img src="public/images/virus.png" alt="virus" width="60px;"></td>
-      <td class="align-middle">Le virus Asiatique</td>
-      <td class="align-middle">200</td>
-      <td class="align-middle"><a href=""class="btn btn-warning">Modifier</a></td>
-      <td class="align-middle"><a href=""class="btn btn-danger">Supprimer</a></td>
-    </tr>
+    <?php endfor; ?>
+
   </tbody>
 </table>
 <a href=""class="btn btn-success d-block">Ajouter</a>
